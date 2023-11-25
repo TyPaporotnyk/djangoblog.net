704d55849b47b2b4e065afd260997f3db1efdf70
Daniil <39118675+TyPaporotnyk@users.noreply.github.com>
2023-11-25 23:34:35 +0200

    Test coommit

--- app/settings/production.py
+++ app/settings/production.py
@@ -6,11 +6,8 @@ SECRET_KEY = "django-insecure-73p2%6_@-*l2cgtm#3p9l_wem^hf!mtgr3hmas&1pk_el_)c*e
 
 ALLOWED_HOSTS = ["*"]
 CSRF_TRUSTED_ORIGINS = [
-    "http://127.0.0.1:3000",
-    "http://localhost:3000",
-    "http://176.117.76.184:80",
-    "http://176.117.76.184:433",
-    "http://176.117.76.184",
+    "http://*.djangoblog.net",
+    "https://*.djangoblog.net",
 ]
 
 EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
